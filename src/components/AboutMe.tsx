bbd29f69981608e576069920065d8f6c4013191f
justinguuu <justin.gu@gmlx.us.com>
2025-02-07 11:02:43 -0500

    timeline implementation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -24,7 +24,7 @@ const AboutMe: React.FC = () => {
             <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-1">Justin Gu</h1>
             <p className="text-blue-600 dark:text-blue-400 mb-4">@JungGu</p>
             <p className="text-gray-700 dark:text-gray-300 mb-4 leading-relaxed">
-              Hey there, I&apos;m Justin, a Software Developer based in Duluth, Georgia, with a passion for desigining, building and managing software solutions. My experience spans from start ups to mid-sized companies, equipping me with a diverse skill set and the ability to adapt seemlessly to different challenges.With an eye for detail and a hunger for learning, I&apos;m all about delivering high-quality solutions that meet both user needs and business objectives.
+              Hey there, I&apos;m Jung aka Justin, a Software Developer based in Duluth, Georgia, with a passion for desigining, building and managing software solutions. My experience spans from start ups to mid-sized companies, equipping me with a diverse skill set and the ability to adapt seemlessly to different challenges.With an eye for detail and a hunger for learning, I&apos;m all about delivering high-quality solutions that meet both user needs and business objectives.
             </p>
             <p className="text-gray-700 dark:text-gray-300 mb-4 leading-relaxed">
               AI? Oh, that&apos;s my jam. I geek out on building software that taps into AI magic.
